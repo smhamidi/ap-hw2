5153728eeea855053997ea795d314be5bffee20f
smhamidi <1380hamidi@gmail.com>
2022-11-12 16:45:57 +0330

    Adding insert function and running all related tests

--- include/trie.h
+++ include/trie.h
@@ -1,6 +1,38 @@
 #ifndef TRIE_H
 #define TRIE_H
 
+#include <functional>
+#include <iostream>
+#include <string>
+#include <vector>
 
+class Trie {
+public:
+  // Node class
+
+  class Node {
+  public:
+    Node(char _data, bool _is_finished);
+    Node();
+
+    std::vector<Node *> children;
+    char data;
+    bool is_finished;
+  };
+  Node *root;
+
+  Trie();
+  // ~Trie();
+  // Trie(const Trie &trie);
+  // Trie(Trie &&trie);
+
+  void insert(std::string str);
+  // bool search(std::string query);
+  // void bfs(std::function<void(Node *&node)> func);
+  // void operator=(const Trie &trie);
+  // void operator=(Trie &&trie);
+
+private:
+};
 
 #endif // TRIE_H
--- src/trie.cpp
+++ src/trie.cpp
@@ -1,1 +1,45 @@
-#include "trie.h"
+#include "../include/trie.h"
+
+Trie::Node::Node(char _data, bool _is_finished)
+    : data{_data}, is_finished{_is_finished} {}
+Trie::Node::Node() {
+  is_finished = false;
+  data = '\0';
+}
+
+Trie::Trie() { root = new Node(); }
+
+void Trie::insert(std::string str) {
+
+  Node *ourNode{this->root};
+
+  for (size_t i = 0; i < str.size(); i++) {
+    bool FlagExist = false;
+    size_t indexIfExist{};
+    for (size_t j = 0; j < ourNode->children.size(); j++) {
+      if (str[i] == ourNode->children[j]->data) {
+        FlagExist = true;
+        indexIfExist = j;
+      }
+    }
+
+    if (FlagExist == false) {
+      Node *addNode{new Node{str[i], false}};
+      ourNode->children.push_back(addNode);
+      size_t ourNodeSize{ourNode->children.size()};
+      ourNode = ourNode->children[ourNodeSize - 1];
+    } else {
+      ourNode = ourNode->children[indexIfExist];
+    }
+  }
+  ourNode->is_finished = true;
+}
+
+// Trie::~Trie() {
+//   if (root == nullptr)
+//     return;
+//   std::vector<Node *> nodes;
+//   this->bfs([&nodes](Trie::Node *&node) { nodes.push_back(node); });
+//   for (const auto &node : nodes)
+//     delete node;
+// }
